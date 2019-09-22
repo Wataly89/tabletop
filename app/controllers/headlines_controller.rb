0e1d26aef45c4d559e8c405024ef207a263071c1
Alyse <awatton89@gmail.com>
2019-09-22 21:55:04 +0000

    simpleform headlines

--- app/controllers/headlines_controller.rb
+++ app/controllers/headlines_controller.rb
@@ -1,5 +1,21 @@
 class HeadlinesController < ApplicationController
 	def index
-		@headline = Headline.order("RANDOM()").first
+		@headlines = Headline.order("RANDOM()").first
 	end
+
+	def new
+		@headline = Headline.new
+	end
+
+	def create
+		Headline.create(headline_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def headline_params
+		params.require(:headline).permit(:quote, :player)			
+	end
+
 end
